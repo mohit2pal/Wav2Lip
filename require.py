1f2e049195e8ae9cbdb8df7f3c34bd6e426c8856
SUBHA BAL PAL <mohit2pal@gmail.com>
2023-07-20 06:04:03 +0530

    codes added

--- require.py
+++ require.py
@@ -10,7 +10,7 @@ os.system('mkdir /content/sample_data')
 os.system('git clone https://github.com/justinjohn0306/Wav2Lip')
 
 # Download the wav2lip_gan.pth checkpoint file
-os.system('curl -o \'Wav2Lip/checkpoints/wav2lip_gan.pth\' \'https://iiitaphyd-my.sharepoint.com/personal/radrabha_m_research_iiit_ac_in/_layouts/15/download.aspx?share=EdjI7bZlgApMqsVoEUUXpLsBxqXbn5z8VTmoxp55YNDcIA\'')
+os.system('wget "https://iiitaphyd-my.sharepoint.com/personal/radrabha_m_research_iiit_ac_in/_layouts/15/download.aspx?share=EdjI7bZlgApMqsVoEUUXpLsBxqXbn5z8VTmoxp55YNDcIA" -O "Wav2Lip/checkpoints/wav2lip_gan.pth"')
 
 # Install the ghc package
 os.system('pip install https://raw.githubusercontent.com/AwaleSajil/ghc/master/ghc-1.0-py3-none-any.whl')
@@ -20,7 +20,7 @@ os.chdir('Wav2Lip')
 os.system('pip install -r requirements.txt')
 
 # Download the s3fd.pth file
-os.system('curl -O "Wav2Lip/face_detection/detection/sfd/s3fd.pth" "https://www.adrianbulat.com/downloads/python-fan/s3fd-619a316812.pth"')
+os.system('wget "https://www.adrianbulat.com/downloads/python-fan/s3fd-619a316812.pth" -O "face_detection/detection/sfd/s3fd.pth"')
 
 # Install the ffmpeg-python package
 os.system('pip install ffmpeg-python')
